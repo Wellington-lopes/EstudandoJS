2921b1a85b75577b0d057dba4f3bffbbb978dd69
Wellington Lopes <wellington.slopes1991@gmail.com>
2023-09-11 10:29:30 -0300

    Add files via upload

--- Nova pasta/script.js
+++ Nova pasta/script.js
@@ -1,9 +1,11 @@
 //Como remover de uma lista usando o indexOF sem precisar saber o local exato 
 
 let alunos = ["Well", "Carol", "Richard", "Rayane"];
-if (alunos.includes("Rayane") === true){
+
+if (alunos.includes("Rayane")){
     alunos.splice(alunos.indexOf("Rayane"), 1);
 }
 
 console.log(alunos);
+console.log(alunos.length)
 
--- Nova pasta/teste.js
+++ Nova pasta/teste.js
@@ -42,7 +42,7 @@ console.log(alunos);
 
 
 //para verificar se o nome já está na lista/array
-
+if (!alunos.nomes.includes("Igor")) // pode escrever das duas formas
 if (alunos.nomes.includes("Igor") === false){
     alunos.nomes.push("Igor");
     alunos.idades.push(18)
@@ -55,8 +55,9 @@ console.log(alunos)
 //Como remover de uma lista usando o indexOF sem precisar saber o local exato 
 
 let alunos = ["Well", "Carol", "Richard", "Rayane"];
+if (alunos.includes("Rayane"){}// pode escrever das duas formas
 if (alunos.includes("Rayane") === true){
     alunos.splice(alunos.indexOf("Rayane"), 1);
 }
-
+console.log(alunos.length);//ver a quantidade dentro de uma lista/array
 console.log(alunos);
